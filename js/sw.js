328ff489ab3f4cd472394423cf91f1b6dfef4834
Vladimir <vladimir.gartsev@gmail.com>
2017-02-16 11:36:23 -0500

    Create sw.js

---
+++ js/sw.js
@@ -0,0 +1,11 @@
+self.addEventListener('install', function(event) {
+// console.log('installed');
+//  event.waitUntil(
+//    fetchStuffAndInitDatabases()
+//  );
+});
+
+self.addEventListener('activate', function(event) {
+// console.log('activated');
+  // You're good to go!
+});
